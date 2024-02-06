875d9551f2cd5b3c233bea9122a5f9aa043d95b7
DanielRS - armlock <135665475+Daniel-Baiano@users.noreply.github.com>
2024-02-06 14:54:02 -0300

    1.introducao_e_vetores .R

--- 1.introducao_e_vetores .R
+++ 1.introducao_e_vetores .R
@@ -10,6 +10,7 @@
 8/2         #divisao
 2^5         #exponenciacao
 
+
 # Avaliacao logica
 3==5	      #igual
 3!=5	      #diferente
@@ -18,6 +19,8 @@
 3>=5	      #maior ou igual
 3<=5	      #menor ou igual
 
+# Observação: Operadores lógicos como `==`, `!=`, `>`, `<`, `>=` e `<=` são usados 
+#para comparações.
 
 # Operacoes concatenadas
 ((2+5-3)*10)^4/7^4
@@ -282,3 +285,100 @@ m
 ar<-array(1:200, c(5, 10, 4))    #vetores multidimensionais
 ar
 ar[,,1]                          #[linha, coluna, matrix]
+
+
+######################################
+##            data.frame            ##
+######################################
+
+#foramdo por vetores de mesmo tamanho, organizados como colunas
+
+number<-c(1, 2, 3, 4, 5)
+letter<-c("x", "y", "z", "w", "j")
+logical<- c(TRUE, FALSE, FALSE, TRUE, FALSE)
+seq<-1:10
+
+dt<-data.frame(number, letter, logical)
+class(dt)
+dt$letter                     #usamos $ para acessar as colunas de um data.frame
+class(dt$letter)              #vetores de caracter sao interpretados como fatores
+
+dt<-data.frame(number, letter, logical, stringsAsFactors = F) #stringsAsFactors = F
+dt$letter
+class(dt$letter)
+
+attributes(dt)                #data.frames possuem colnames e rownames
+colnames(dt)
+row.names(dt)
+
+dt[5,2]                       #acessamos data.frames da mesma forma que matrizes
+
+######################################
+##    Trabalhando com data.frames   ##
+######################################
+
+#para acessar data.frames usamos [], [[]] ou $
+
+dt<-data.frame(number=c(1, 2, 3, 4, 5), 
+               letter = c("x", "y", "z", "w", "j"),
+               logical = c(TRUE, FALSE, FALSE, TRUE, FALSE))
+
+dt[[1]]              # [[ ]] acessa cada coluna por posicao
+dt[,1]               # [ ] acessa as coordenaas [linha, coluna]
+dt$number            # $ acessa cada coluna por nome
+
+d
+cars<-mtcars        #carrega o data.frame mtcars
+head(cars)          #mostra as 6 primeiras linhas
+tail(cars)          #mostra as 6 ultimas linhas
+
+#faca um data.frame somente com as informacoes dos carros com mais de 100hp
+
+cars_100 <- cars[cars$hp > 100,]
+
+#data.frames possuem colnames e rownames
+colnames(dt)
+row.names(dt)
+
+#podemos alterar colanmes e rownames
+row.names(dt)<-c("a", "b", "c", "d", "e")
+colnames(dt)[2]<-"letras"     #alteramos unicamente a posicao 2
+
+#podemos alterar valores especificos de um data.frame
+dt[3,1]<-"10"
+dt$logical<-as.numeric(dt$logical)
+dt$letras<-NA
+
+#Podemos verificar as ocorrencias de um data.frame em outro
+
+biometria<-data.frame(nomes=c("Carlos", "Roberto", "Olivio", "Joel"),
+                      altura=c(180, 187, 155, 168),
+                      peso=c(80, 90, 98, 64))
+esportes<-data.frame(nomes=c("Carlos", "Roberto", "Olivio", "Jomar"),
+                     esportes=c("futebol", "remo", "sumo", "maratona"))
+
+biometria$nomes%in%esportes$nomes         #constroi um logico 
+idx<-biometria$nomes%in%esportes$nomes    #pode ser usado como indice
+x<-biometria[idx,]                        
+
+y <- esportes[esportes$nomes%in%biometria$nomes,]
+
+#ordenando data.frames por uma coluna
+#opcao1
+biometria<-biometria[with(biometria, order(altura)), ]
+#opcao2
+biometria <- biometria[order(biometria$altura),]
+
+#ordene o data.frame cars com a coluna "mpg" em ordem crescente
+cars_order <- cars[with(cars, order(mpg)),]
+
+#unindo data.frames com a funcao merge()
+
+unido<-merge(biometria, esportes, by="nomes")   #OBS.:Independe da ordem dos data.frames!
+#A busca eh feita pelo nome, nao pela ordem!
+#O resultado sempre virah em ordem alfabetica!
+
+unido<-merge(biometria, esportes, by="nomes", all.x=T)#com todos presentos no primeiro
+unido<-merge(biometria, esportes, by="nomes", all.y=T)#com todos presentos no segundo
+unido<-merge(biometria, esportes, by="nomes", all=T)#com todos presentos
+#OBS.: nesse caso, as informacoes faltantes sao preenchidas por NA!
